c1155a7b5b2ac99062df18a6590cb4c9bfad9f7d
Crystal <dehuihu@gmail.com>
2025-02-27 19:09:58 +0000

    Add application_form and new route to handle submitted form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from database import load_jobs_from_db, load_job_from_db
 from sqlalchemy import text
 
@@ -23,6 +23,14 @@ def show_job(id):
   if not job:
     return "Not Found", 404
   return render_template('jobpage.html', job=job)
-  
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  #store this info into db
+  #display acknowledgement
+  return jsonify(data)
+
+
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
